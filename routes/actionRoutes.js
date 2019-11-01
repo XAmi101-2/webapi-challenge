89f5c8bc3cd3eb40cab5b7be83579ec2b3a4b33d
XAmi101 <famousvacancy@gmail.com>
2019-11-01 11:58:03 -0400

    routes wip.

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -28,5 +28,48 @@ router.get('/:id', (req, res) => {
 
 
 
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    if (Action) {
+        Action.remove(id)
+            .then(action => res.status(200).json({
+                message: 'The action has been deleted'
+            }))
+            .catch(() =>
+                res
+                .status(500)
+                .json({
+                    error: "Unable to delete action"
+                })
+            );
+    } else {
+        res.status(400).json({
+            message: "The action with the specified ID does not exist."
+        });
+
+    }
+});
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const changes = req.body;
+    if (!req.body.project_id || !req.body.description || !req.body.notes || req.body.completed === undefined) {
+        res.status(400).json({
+            message: "missing data field, please check you have  a project_id, notes, description, and a completed with a true or false"
+        })
+    } else {
+        Action.update(id, changes)
+            .then(action => res.status(200).json(action))
+            .catch(() =>
+                res.status(500).json({
+                    errorMessage: "action could not be updated"
+                })
+            );
+    }
+});
+
+
+
+
 
 module.exports = router;
